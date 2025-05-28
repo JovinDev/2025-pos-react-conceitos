af6892efa4370481e64877f80c720b6a82580682
João Vinicius Silva dos Santos <138674913+JovinDev@users.noreply.github.com>
2025-05-28 22:42:56 +0000

    Atualiza estilo da home e ajusta README

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
+// src/app/page.tsx
 "use client";
 
-const Home = () => {
-  return (
-    <div>
-      <main>
-        <h1>React - conceitos básicos</h1>
-      </main>
-    </div>
-  );
+import React from "react";
+
+const Titulo = () => <h1>React - Conceitos básicos</h1>;
+
+const SubTitulo = () => <h2>Lista de tarefas</h2>;
+
+function Cabecalho() {
+	return (
+		<>
+			<Titulo />
+			<SubTitulo />
+		</>
+	);
 }
+
+class Tarefa extends React.Component {
+	render(): React.ReactNode {
+		return (
+			<div>
+				<h3>Codar e codar durante 2025</h3>
+				<p>Pendente</p>
+			</div>
+		);
+	}
+}
+const Home = () => {
+	return (
+		<>
+			<Cabecalho />
+			<Tarefa />
+		</>
+	);
+};
 
 export default Home;
